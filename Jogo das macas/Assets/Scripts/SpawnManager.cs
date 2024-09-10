cfc031b532e26681c15c577e657d5b4a8d757579
zaikodan <zaikodan@gmail.com>
2024-09-10 09:46:16 -0300

    Update SpawnManager.cs

--- Jogo das macas/Assets/Scripts/SpawnManager.cs
+++ Jogo das macas/Assets/Scripts/SpawnManager.cs
@@ -4,5 +4,41 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    
+    [SerializeField] GameObject[] applePrefabs;
+
+    float timer;
+    const float cooldown = 1;
+
+    private void Update()
+    {
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        timer = Time.time;
+
+        if(timer <= 0)
+        {
+            float appleIndex = Random.Range(0, 1);
+
+            GameObject appleSelected = null;
+
+            switch(appleIndex)
+            {
+                case <= 50:
+                    appleSelected = applePrefabs[0];
+                    break;
+                case <= 80:
+                    appleSelected = applePrefabs[1];
+                    break;
+                case > 80:
+                    appleSelected = applePrefabs[2];
+                    break;
+            }
+
+            Instantiate(appleSelected, new Vector3(Random.Range(-GameManager.instance.ScreenBounds.x, GameManager.instance.ScreenBounds.x), GameManager.instance.ScreenBounds.y), Quaternion.identity);
+            timer = cooldown;
+        }
+    }
 }
